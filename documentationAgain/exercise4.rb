23fb9a6e995afc1bce1feb36f3e02e97438afbb4
Christopher Perkins <Christopher.Perkins@null.net>
2023-01-09 11:15:46 -0500

    Documentation Again: Exercise 4 complete

---
+++ documentationAgain/exercise4.rb
@@ -0,0 +1,16 @@
+# The Array#bsearch method is used to search ordered Arrays more quickly than 
+# #find and #select can. Assume you have the following code:
+# 
+# a = [1, 4, 8, 11, 15, 19]
+# 
+# How would you search this Array to find the first element whose value exceeds 
+# 8?
+
+a = [1, 4, 8, 11, 15, 19]
+
+# from https://docs.ruby-lang.org/en/2.7.0/Array.html#method-i-bsearch
+# bsearch {|x| block } → elem 
+# The minimum mode requires the block to require a true or false return value. 
+# As such it will return the first value that returns true.
+
+puts a.bsearch { |x| x > 8 }
