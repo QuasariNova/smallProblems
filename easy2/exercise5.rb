8c83c950a97aad48e7132d3f15be2b00feeac3a7
Christopher Perkins <Christopher.Perkins@null.net>
2023-01-14 10:39:09 -0500

    Easy2: Exercise 5 Further Exploration

--- easy2/exercise5.rb
+++ easy2/exercise5.rb
@@ -9,12 +9,41 @@
 # What is your name? Bob!
 # HELLO BOB. WHY ARE WE SCREAMING?
 
-print "What is your name?"
+# Original Answer:
+# print "What is your name?"
+# name = gets.chomp
+# 
+# output = if name.ends_with? '!'
+#            "HELLO #{name.chop.upcase}. WHY ARE WE SCREAMING?"
+#          else
+#            "Hello #{name}."
+#          end
+# puts ouptut
+#
+# Their solution uses an -1 index on the string. I forgot you could do this to
+# chose characters from the end. 
+# The Further exploration wants me to modify their solution to use 
+# String#chomp! and String#chop! each
+#
+# String#chop!:
+# print 'What is your name? '
+# name = gets.chomp
+# 
+# if name[-1] == '!'
+#   name.chop!
+#   puts "HELLO #{name.upcase}. WHY ARE WE SCREAMING?"
+# else
+#   puts "Hello #{name}."
+# end
+#
+# String#chomp!
+
+print 'What is your name? '
 name = gets.chomp
 
-output = if name.ends_with? '!'
-           "HELLO #{name.chop.upcase}. WHY ARE WE SCREAMING?"
-         else
-           "Hello #{name}."
-         end
-puts ouptut
+if name[-1] == '!'
+  name.chomp! '!'
+  puts "HELLO #{name.upcase}. WHY ARE WE SCREAMING?"
+else
+  puts "Hello #{name}."
+end
