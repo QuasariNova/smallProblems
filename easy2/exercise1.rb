1683ee1b42ad254ef3c970a6eb60bdeafac677a2
Christopher Perkins <Christopher.Perkins@null.net>
2023-01-14 08:20:08 -0500

    Easy 1: Exercise 1 further exploration complete

--- easy2/exercise1.rb
+++ easy2/exercise1.rb
@@ -4,6 +4,19 @@
 # Example Output
 # 
 # Teddy is 69 years old!
+#
+# Original answer:
+# age_range = Array(20..200)
+# puts "Teddy is #{age_range.sample} years old!"
+#
+# The solution used rand(20..200) instead of Array(20..200).sample
+# 
+# Further Exploration:
+# Modify this program to ask for a name, and then print the age for that
+# person. For an extra challenge, use "Teddy" as the name if no name is entered.
 
-age_range = Array(20..200)
-puts "Teddy is #{age_range.sample} years old!"
+puts '>> What is your name?'
+print '>> '
+name = gets.chomp
+name = 'Teddy' if name.empty?
+puts "#{name} is #{rand(20..200)} years old!"
