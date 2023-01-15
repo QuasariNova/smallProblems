c77d48490c11fa536182d5213b4c1a0958cd6798
Christopher Perkins <Christopher.Perkins@null.net>
2023-01-15 09:17:59 -0500

    Easy 3: Exercise 7 Further Exploration answered

--- easy3/exercise7.rb
+++ easy3/exercise7.rb
@@ -8,14 +8,51 @@
 #   palindrome?('Madam') == false          # (case matters)
 #   palindrome?("madam i'm adam") == false # (all characters matter)
 #   palindrome?('356653') == true
+#
+# Original Answer
+# def palindrome?(string)
+#   half = string.size / 2
+#   half.times { |n| return false unless string[n] == string[-(n+1)] }
+#   true
+# end
+#
+# puts palindrome?('madam') == true
+# puts palindrome?('Madam') == false          # (case matters)
+# puts palindrome?("madam i'm adam") == false # (all characters matter)
+# puts palindrome?('356653') == true
+#
+# Their solution:
+# def palindrome?(string)
+#   string == string.reverse
+# end
+#
+# Wow, duh. It was easier to do that, but now I'm more prepared for:
+# Further Exploration
+#
+# Write a method that determines whether an array is palindromic; that is, the
+# element values appear in the same sequence both forwards and backwards in the
+# array. Now write a method that determines whether an array or a string is
+# palindromic; that is, write a method that can take either an array or a
+# string argument, and determines whether that argument is a palindrome. You
+# may not use an if, unless, or case statement or modifier.
+#
+# My original answer technically satisfies this all three, but it is asking me
+# to not use any conditional, so let's try that
 
-def palindrome?(string)
-  half = string.size / 2
-  half.times { |n| return false unless string[n] == string[-(n+1)] }
-  true
+def palindrome?(array_or_string)
+  half = array_or_string.size / 2
+  is_palindrome = true
+  half.times do |n|
+    same = array_or_string[n] == array_or_string[-(n+1)]
+    is_palindrome = is_palindrome && same
+  end
+  is_palindrome
 end
 
 puts palindrome?('madam') == true
 puts palindrome?('Madam') == false          # (case matters)
 puts palindrome?("madam i'm adam") == false # (all characters matter)
 puts palindrome?('356653') == true
+puts palindrome?([0, 1, 2, 3]) == false
+puts palindrome?(['n', 'o', 3, 'o', 'n'])
+puts palindrome?([1, 2, 3, 3, 2, 1])
