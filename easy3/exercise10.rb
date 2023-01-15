9d47fce9e8a30a1d10d0eb55a6f84715a59d40cd
Christopher Perkins <Christopher.Perkins@null.net>
2023-01-15 10:33:59 -0500

    Easy 3: Exercise 10 further exploration done

--- easy3/exercise10.rb
+++ easy3/exercise10.rb
@@ -22,3 +22,17 @@ puts uppercase?('Four Score') == false
 puts uppercase?('FOUR SCORE') == true
 puts uppercase?('4SCORE!') == true
 puts uppercase?('') == true
+
+# The Solution uses just a upcase check without deleting nonalphabetic
+# characters. That makes sense, as you can't upcase the other characters, me
+# deleting them was superfluous.
+#
+# Further Exploration
+#
+# Food for thought: in our examples, we show that uppercase? should return true
+# if the argument is an empty string. Would it make sense to return false
+# instead? Why or why not?
+#
+# You could make the argument either way. Yes there are no alphabet characters
+# that are uppercase, but it is true that all of them are in absense. Depends
+# on how this will be used.
