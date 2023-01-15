0854b0858c3b6a2680c6e4224a384aea4bc5ab97
Christopher Perkins <Christopher.Perkins@null.net>
2023-01-15 10:24:29 -0500

    Easy 3: Exercise 9 Further exploration answered

--- easy3/exercise9.rb
+++ easy3/exercise9.rb
@@ -20,3 +20,15 @@ puts palindromic_number?(34543) == true
 puts palindromic_number?(123210) == false
 puts palindromic_number?(22) == true
 puts palindromic_number?(5) == true
+
+# Further Exploration
+#
+# Suppose your number begins with one or more 0s. Will your method still work?
+# Why or why not? Is there any way to address this?
+
+# Our method won't work if you consider precluding 0s to offset 0s at the end.
+# Technically, all numbers have preceding 0s, so if you wanted to include this
+# in the check if it's palindromic, you just have to skip all 0s at the end of
+# the number and only check the preceding value. Overall, that's kind of dumb,
+# because it's still not palindromic if you consider one side has infinite 0s
+# and the other finite.
