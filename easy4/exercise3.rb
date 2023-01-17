d521108ba4f0a26018ed24bae098784277f274b8
Christopher Perkins <Christopher.Perkins@null.net>
2023-01-17 10:49:34 -0500

    Easy 4: Exercise 3 further exploration done

--- easy4/exercise3.rb
+++ easy4/exercise3.rb
@@ -21,6 +21,54 @@
 # leap_year?(100) == false
 # leap_year?(400) == true
 
-def leap_year?(year)
+# original answer
+# ef leap_year?(year)
+#   if year % 100 == 0
+#     year % 400 == 0
+#   else
+#     year % 4 == 0
+#   end
+# end
 
+# Further exploration
+# The order in which you perform tests for a leap year calculation is
+# important. For what years will leap_year? fail if you rewrite it as:
+#
+# def leap_year?(year)
+#   if year % 100 == 0
+#     false
+#   elsif year % 400 == 0
+#     true
+#   else
+#     year % 4 == 0
+#   end
+# end
+#
+# Any year dividable by 400 would always return false
+#
+# Further Exploration 2
+# Can you rewrite leap_year? to perform its tests in the opposite order of the
+# above solution? That is, test whether the year is divisible by 4 first, then,
+# if necessary, test whether it is divisible by 100, and finally, if necessary,
+# test whether it is divisible by 400. Is this solution simpler or more complex
+# than the original solution?
+
+def leap_year?(year)
+  return false unless year % 4 == 0
+  return year % 400 if year % 100 == 0
+  true
 end
+
+puts leap_year?(2016) == true
+puts leap_year?(2015) == false
+puts leap_year?(2100) == false
+puts leap_year?(2400) == true
+puts leap_year?(240000) == true
+puts leap_year?(240001) == false
+puts leap_year?(2000) == true
+puts leap_year?(1900) == false
+puts leap_year?(1752) == true
+puts leap_year?(1700) == false
+puts leap_year?(1) == false
+puts leap_year?(100) == false
+puts leap_year?(400) == true
