b44f6456760d8bdeb7fbeeed3ed17fbfb42ff0b0
Christopher Perkins <Christopher.Perkins@null.net>
2023-01-17 11:16:24 -0500

    Easy 4: Exercise 5 Further Exploration done

--- easy4/exercise5.rb
+++ easy4/exercise5.rb
@@ -11,11 +11,29 @@
 # multisum(5) == 8
 # multisum(10) == 33
 # multisum(1000) == 234168
+#
+# original answer:
+# def multisum(number)
+#   threes_fives = []
+#   1.upto(number) { |n| threes_fives << n if n % 3 == 0 || n % 5 == 0 }
+#   threes_fives.sum
+# end
+#
+# Further Exploration
+#
+# Investigate Enumerable.inject (also known as Enumerable.reduce), How might
+# this method be useful in solving this problem? (Note that Enumerable methods
+# are available when working with Arrays.) Try writing such a solution. Which
+# is clearer? Which is more succinct?
+
+def multiple?(number, divisor)
+  number % divisor == 0
+end
 
 def multisum(number)
-  threes_fives = []
-  1.upto(number) { |n| threes_fives << n if n % 3 == 0 || n % 5 == 0 }
-  threes_fives.sum
+  1.upto(number).inject(0) do |sum, n|
+    multiple?(n, 3) || multiple?(n, 5) ? sum + n : sum
+  end
 end
 
 puts multisum(3) == 3
