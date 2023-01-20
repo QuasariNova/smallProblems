0548a132c48a315d300315fa35cd481511d0a8d6
Christopher Perkins <Christopher.Perkins@null.net>
2023-01-20 12:14:50 -0500

    Easy 5: Exercise 9 answered

---
+++ easy5/exercise9.rb
@@ -0,0 +1,30 @@
+# Write a method that takes a string argument and returns a new string that
+# contains the value of the original string with all consecutive duplicate
+# characters collapsed into a single character. You may not use String#squeeze
+# or String#squeeze!.
+#
+# Examples:
+#
+# crunch('ddaaiillyy ddoouubbllee') == 'daily double'
+# crunch('4444abcabccba') == '4abcabcba'
+# crunch('ggggggggggggggg') == 'g'
+# crunch('a') == 'a'
+# crunch('') == ''
+
+def crunch(string)
+  crunched_string = ''
+  index = 0
+
+  until index == string.length
+    crunched_string << string[index] unless crunched_string[-1] == string[index]
+    index += 1
+  end
+
+  crunched_string
+end
+
+puts crunch('ddaaiillyy ddoouubbllee') == 'daily double'
+puts crunch('4444abcabccba') == '4abcabcba'
+puts crunch('ggggggggggggggg') == 'g'
+puts crunch('a') == 'a'
+puts crunch('') == ''
