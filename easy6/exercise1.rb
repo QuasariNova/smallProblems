5f173a715b662c678e0fa81d4b6f2f4712ba8e61
Christopher Perkins <Christopher.Perkins@null.net>
2023-02-02 17:50:19 -0500

    Easy 6: Exercise 1 Further Exploration

--- easy6/exercise1.rb
+++ easy6/exercise1.rb
@@ -63,7 +63,7 @@ end
 # p float_to_angle(93.034773) == [93, 2, 5]
 
 def dms(angle)
-  arr = float_to_angle angle
+  arr = float_to_angle(angle % 360)
   format("%d#{DEGREE}%02d'%02d\"", arr[0], arr[1], arr[2])
 end
 
@@ -73,3 +73,11 @@ puts dms(254.6) #== %(254°36'00") Rounding error
 p dms(93.034773) == %(93°02'05")
 p dms(0) == %(0°00'00")
 p dms(360) == %(360°00'00") || dms(360) == %(0°00'00")
+
+# Since degrees are normally restricted to the range 0-360, can you modify the
+# code so it returns a value in the appropriate range when the input is less
+# than 0 or greater than 360?
+
+p dms(400) == %(40°00'00")
+p dms(-40) == %(320°00'00")
+p dms(-420) == %(300°00'00")
