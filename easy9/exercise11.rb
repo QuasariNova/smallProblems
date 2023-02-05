b19ce102ab03701399bdf01444d8821cf043d2df
Christopher Perkins <Christopher.Perkins@null.net>
2023-02-05 12:19:00 -0500

    Easy 9: Exercise 11 Further Exploration

--- easy9/exercise11.rb
+++ easy9/exercise11.rb
@@ -33,9 +33,36 @@ def oddities(arr)
   arr.select.with_index { |_, i| i.even? }
 end
 
-p oddities([2, 3, 4, 5, 6]) == [2, 4, 6]
-p oddities([1, 2, 3, 4, 5, 6]) == [1, 3, 5]
-p oddities(['abc', 'def']) == ['abc']
-p oddities([123]) == [123]
-p oddities([]) == []
-p oddities([1, 2, 3, 4, 1]) == [1, 3, 1]
+# p oddities([2, 3, 4, 5, 6]) == [2, 4, 6]
+# p oddities([1, 2, 3, 4, 5, 6]) == [1, 3, 5]
+# p oddities(['abc', 'def']) == ['abc']
+# p oddities([123]) == [123]
+# p oddities([]) == []
+# p oddities([1, 2, 3, 4, 1]) == [1, 3, 1]
+
+# Further Exploration
+# Write a companion method that returns the 2nd, 4th, 6th, and so on elements
+# of an array.
+
+# Try to solve this exercise in at least 2 additional ways.
+
+def interleave(arr, evens:false)
+  arr.select.with_index { |_, i| evens ? i.odd? : i.even? }
+end
+
+def interleave2(arr, evens:false)
+  arr.each_with_index.with_object([]) do |(ele, i), out|
+    out << ele if evens && i.odd? || !evens && i.even?
+  end
+end
+
+def interleave3(arr, evens:false)
+  arr.dup.delete_if.with_index { |_, i| evens ? i.even? : i.odd? }
+end
+
+p interleave([2, 3, 4, 5, 6]) == [2, 4, 6]
+p interleave([1, 2, 3, 4, 5, 6], evens: true) == [2, 4, 6]
+p interleave2([2, 3, 4, 5, 6]) == [2, 4, 6]
+p interleave2([1, 2, 3, 4, 5, 6], evens: true) == [2, 4, 6]
+p interleave3([2, 3, 4, 5, 6]) == [2, 4, 6]
+p interleave3([1, 2, 3, 4, 5, 6], evens: true) == [2, 4, 6]
