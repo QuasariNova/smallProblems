def4e6ccfb364965e19de483b2cb73046656b21e
Christopher Perkins <Christopher.Perkins@null.net>
2023-02-08 19:53:05 -0500

    Medium 1: Exercise 1 Further Exploration

--- medium1/exercise1.rb
+++ medium1/exercise1.rb
@@ -36,10 +36,32 @@ def rotate_array(arr)
   arr[1..-1] + [arr.first]
 end
 
-p rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
-p rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
-p rotate_array(['a']) == ['a']
+# p rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
+# p rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
+# p rotate_array(['a']) == ['a']
 
-x = [1, 2, 3, 4]
-p rotate_array(x) == [2, 3, 4, 1]   # => true
-p x == [1, 2, 3, 4]                 # => true
+# x = [1, 2, 3, 4]
+# p rotate_array(x) == [2, 3, 4, 1]   # => true
+# p x == [1, 2, 3, 4]                 # => true
+
+# Further exploration
+# Write a method that rotates a string instead of an array. Do the same thing
+# for integers. You may use rotate_array from inside your new method.
+def rotate_string(str)
+  rotate_array(str.chars).join
+end
+
+def rotate_integer(int)
+  rotate_string(int.to_s).to_i
+end
+
+p rotate_string('Launch School') == 'aunch SchoolL'
+p rotate_string('a') == 'a'
+
+x = 'string'
+p rotate_string(x) == 'trings'
+p x == 'string'
+
+p rotate_integer(10132) == 1321
+p rotate_integer(98765) == 87659
+p rotate_integer(1) == 1
